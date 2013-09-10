12d27bac5b22fb9d9d08c8e13b9d5d4bcd531a0e
Braden McDorman <bmcdorman@gmail.com>
2013-09-10 01:03:55 -0500

    Updates

--- common/pre_package.sh
+++ common/pre_package.sh
@@ -72,56 +72,6 @@ if [[ ${OS_NAME} == "Darwin" ]]; then
 fi
 
 
-###########
-#   ks2   #
-###########
-
-KS2_BASE="${PACKAGE}/ks2"
-KS2_EXTRAS="${KS2_BASE}"
-
-# Mac OS X Bundle
-if [[ ${OS_NAME} == "Darwin" ]]; then
-	KS2_EXTRAS="${KS2_BASE}/ks2.app/Contents"
-fi
-
-# Install Base
-mkdir -p ${KS2_BASE}
-cp -r ks2/deploy/* ${KS2_BASE}
-
-# These install the necessary shared libraries
-LIBKOVAN_PACKAGE=$(ls ${BUILD}/libkovan-*)
-LIBKOVAN_HOST_PACKAGE=$(ls ${BUILD}/libkovan_host-*)
-OPENCV_HOST_PACKAGE=$(ls ${BUILD}/opencv_host-*)
-ZBAR_PACKAGE=$(ls ${BUILD}/zbar-*)
-ZBAR_HOST_PACKAGE=$(ls ${BUILD}/zbar_host-*)
-kissarchive -e ${PCOMPILER_PACKAGE%.*} ${KS2_EXTRAS}
-kissarchive -e ${LIBKAR_PACKAGE%.*} ${KS2_EXTRAS}
-kissarchive -e ${LIBKOVANSERIAL_PACKAGE%.*} ${KS2_EXTRAS}
-kissarchive -e ${ZBAR_HOST_PACKAGE%.*} ${KS2_EXTRAS}
-kissarchive -e ${LIBKOVAN_HOST_PACKAGE%.*} ${KS2_EXTRAS}
-kissarchive -e ${ZBAR_PACKAGE%.*} ${KS2_EXTRAS}/prefix
-kissarchive -e ${LIBKOVAN_PACKAGE%.*} ${KS2_EXTRAS}/prefix
-kissarchive -e ${OPENCV_PACKAGE%.*} ${KS2_EXTRAS}/prefix
-kissarchive -e ${OPENCV_HOST_PACKAGE%.*} ${KS2_EXTRAS}
-
-if [[ ${OS_NAME} == "Darwin" ]]; then
-	# Make libkovan use bundled libzbar
-	framework_path="${PWD}/${PACKAGE}/ks2/ks2.app/Contents/Frameworks"
-	for i in $(ls -1 ${framework_path}/*.dylib)
-        do
-		install_name_tool -change "/usr/local/lib/libzbar.0.dylib" "@executable_path/../Frameworks/libzbar.0.dylib" ${i}
-		install_name_tool -change "lib/libopencv_core.2.4.dylib" "@executable_path/../Frameworks/libopencv_core.2.4.dylib" ${i}
-		install_name_tool -change "lib/libopencv_highgui.2.4.dylib" "@executable_path/../Frameworks/libopencv_highgui.2.4.dylib" ${i}
-		install_name_tool -change "lib/libopencv_imgproc.2.4.dylib" "@executable_path/../Frameworks/libopencv_imgproc.2.4.dylib" ${i}
-		install_name_tool -change "lib/libopencv_imgproc.2.4.dylib" "@executable_path/../Frameworks/libopencv_imgproc.2.4.dylib" ${i}
-        done
-
-	lib_path="${PWD}/${PACKAGE}/ks2/ks2.app/Contents/prefix/usr/lib"
-	for i in $(ls -1 ${lib_path}/*.dylib)
-        do
-		install_name_tool -change "/usr/local/lib/libzbar.0.dylib" "lib/libzbar.0.dylib" ${i}
-	done
-fi
 
 ############
 # KISS IDE #
@@ -143,7 +93,9 @@ else
 fi
 
 mkdir -p ${KISS_EXTRAS}/docs
+mkdir -p ${KISS_EXTRAS}/docs/libkovan
 cp -r link-docs/* ${KISS_EXTRAS}/docs
+cp -r libkovan/doc/* ${KISS_EXTRAS}/docs/libkovan
 
 # These install the necessary shared libraries
 kissarchive -e ${PCOMPILER_PACKAGE%.*} ${KISS_EXTRAS}
